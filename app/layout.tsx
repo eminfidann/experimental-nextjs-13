6f5da5f10f32e72774ff3e0fb21b422f3b323ad4
Emin Fidan <fidanemin42.ef@gmail.com>
2022-10-26 10:33:23 +0300

    Nextjs 13 beta app initialize

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
